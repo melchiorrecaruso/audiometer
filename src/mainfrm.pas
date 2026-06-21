e639e857baa633bee5f0232e767b96ed9d845975
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2026-06-21 14:59:56 +0200

    Updating copyright info

--- src/mainfrm.pas
+++ src/mainfrm.pas
@@ -1,7 +1,7 @@
 {
   Description: Main Form.
 
-  Copyright (C) 2020-2025 Melchiorre Caruso <melchiorrecaruso@gmail.com>
+  Copyright (C) 2020-2026 Melchiorre Caruso <melchiorrecaruso@gmail.com>
 
   This source is free software; you can redistribute it and/or modify it under
   the terms of the GNU General Public License as published by the Free
