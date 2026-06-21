eb6e68a5128b05ea19628548efc0f559ea7587bc
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2026-06-21 14:59:31 +0200

    Updating copyright info

--- src/spectrum.pas
+++ src/spectrum.pas
@@ -1,7 +1,7 @@
 {
   Description: Spectrum routines.
 
-  Copyright (C) 2026 Melchiorre Caruso <melchiorrecaruso@gmail.com>
+  Copyright (C) 2025-2026 Melchiorre Caruso <melchiorrecaruso@gmail.com>
 
   This source is free software; you can redistribute it and/or modify it under
   the terms of the GNU General Public License as published by the Free
